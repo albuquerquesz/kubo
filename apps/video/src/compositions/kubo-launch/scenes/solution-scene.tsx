f4cdc79d5b791160b7fefc9b22e605aee333e310
Victor Albuquerque <albq.victor@gmail.com>
2026-08-04 14:23:32 -0300

    refactor(video): Adjust minHeight of solution scene video

--- apps/video/src/compositions/kubo-launch/scenes/solution-scene.tsx
+++ apps/video/src/compositions/kubo-launch/scenes/solution-scene.tsx
@@ -64,7 +64,7 @@ export const SolutionScene: React.FC<SolutionSceneProps> = ({ command }) => {
             style={{
               width: "100%",
               maxWidth: 980,
-              minHeight: 220,
+              minHeight: 520,
             }}
           />
         </div>
