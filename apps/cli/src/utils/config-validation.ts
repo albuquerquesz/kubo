a574a71086efd7256069ce234ee530011006cea5
Victor Albuquerque <albq.victor@gmail.com>
2026-08-12 19:58:29 -0300

    feat(cli): separate testing setup from addons

--- apps/cli/src/utils/config-validation.ts
+++ apps/cli/src/utils/config-validation.ts
@@ -11,6 +11,7 @@ import {
   validateApiFrontendCompatibility,
   validateExamplesCompatibility,
   validatePaymentsCompatibility,
+  validateTestingAgainstFrontends,
   validateSelfBackendCompatibility,
   validateDockerServerDeploy,
   validateDockerWebDeployDesktopAddons,
@@ -499,6 +500,7 @@ export function validateFullConfig(
     yield* validateBackendConstraints(config, providedFlags, options);
 
     yield* validateFrontendConstraints(config, providedFlags);
+    yield* validateTestingAgainstFrontends(config.testing ?? [], config.frontend ?? []);
 
     yield* validateApiConstraints(config, options);
 
@@ -574,6 +576,7 @@ export function validateConfigForProgrammaticUse(config: Partial<ProjectConfig>)
     }
 
     yield* validateApiFrontendCompatibility(config.api, config.frontend);
+    yield* validateTestingAgainstFrontends(config.testing ?? [], config.frontend ?? []);
 
     yield* validatePaymentsCompatibility(
       config.payments,
